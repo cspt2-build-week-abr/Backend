0b457ac9ab4dd41fa0251fd5c5d7f5961f7c4ece
Joseph <josephryan96@gmail.com>
2019-07-10 20:15:33 -0700

    Created migrations

--- mud/models.py
+++ mud/models.py
@@ -2,7 +2,28 @@ from django.db import models
 from uuid import uuid4
 # Create your models here.
 
-class Room(models.Model):
+class Areas(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    name = models.CharField(max_length=50)
-    items = models.TextField(blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    description = models.CharField(max_length=64, blank=True)
+    pokeballs = models.CharField(max_length=128, blank=True)
+    pokemon = models.CharField(max_length=128, blank=True)
+    adjacents = models.CharField(max_length=4, blank=True)
+
+class Users(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    username = models.CharField(max_length=50, unique=True, blank=True)
+    token = models.CharField(max_length=248, unique=True, blank=True)
+    items = models.CharField(max_length=64, blank=True)
+    area_id = models.IntegerField()
+
+class Pokemon(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=64, unique=True, blank=True)
+    catchChance = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    spawnChance = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+
+class Pokeballs(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=64, unique=True, blank=True)
+    catchRate = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
