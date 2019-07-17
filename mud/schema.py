12a4eb21322fd434c41ee664d83fa3e514780253
Joseph <josephryan96@gmail.com>
2019-07-16 19:37:53 -0700

    added areas to schema

--- mud/schema.py
+++ mud/schema.py
@@ -1,11 +1,23 @@
 from django.conf import settings
 from graphene_django import DjangoObjectType
 import graphene
-from .models import Pokemon, Users
+from .models import Pokemon, Users, Areas
 
 class UserType(DjangoObjectType):
     class Meta:
         model = Users
+        interfaces = (graphene.relay.Node,)
+
+class PokemonType(DjangoObjectType):
+
+    class Meta:
+        model = Pokemon
+        interfaces = (graphene.relay.Node,)
+
+class AreaType(DjangoObjectType):
+
+    class Meta:
+        model = Areas
         interfaces = (graphene.relay.Node,)
 
 class CreateUser(graphene.Mutation):
@@ -28,17 +40,15 @@ class CreateUser(graphene.Mutation):
 class Mutation(graphene.ObjectType,):
     create_user = CreateUser.Field()
 
-class PokemonType(DjangoObjectType):
 
-    class Meta:
-        model = Pokemon
-        interfaces = (graphene.relay.Node,)
 
 class Query(graphene.ObjectType):
 
     allPokemon = graphene.List(PokemonType)
     pokemon = graphene.Field(PokemonType, pokemonId=graphene.String())
     users = graphene.List(UserType)
+    allAreas = graphene.List(AreaType)
+    area = graphene.Field(AreaType, areaId=graphene.String())
 
     def resolve_allPokemon(self, info):
         return Pokemon.objects.all()
@@ -49,6 +59,15 @@ class Query(graphene.ObjectType):
 
     def resolve_users(self, info):
         return Users.objects.all()
+
+    def resolve_allAreas(self, info):
+        return Areas.objects.all()
+
+    def resolve_area(self, info, **kwargs):
+        areaId = kwargs.get('areaId')
+        print(areaId)
+        print(kwargs)
+        return Areas.objects.get(areaId=areaId)
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
